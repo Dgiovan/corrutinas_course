c5f464776e691de7a07f1a5037f3483c2763de4e
KWgiovanny <54039393+KWgiovanny@users.noreply.github.com>
2021-12-23 15:53:17 -0600

    add constructor´s run blocking, launch, Async

--- app/src/main/java/com/ihuman/apps/corrutinas_flow/CoroutinesBasic.kt
+++ app/src/main/java/com/ihuman/apps/corrutinas_flow/CoroutinesBasic.kt
@@ -1,13 +1,59 @@
 package com.ihuman.apps.corrutinas_flow
 
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 
 fun main(){
-   // globalScope()
-    suspendFun()
+    // globalScope()
+    //suspendFun()
+    newTopic("Constructores de corrutinas")
+    //cRunBlocking()
+    //cLaunch()
+    cAsync()
     readLine()
+}
+
+fun cAsync() {
+    /**Async esta diseñado para emitir un resultado*/
+    runBlocking {
+        newTopic("Async")
+        val result =  async {
+            startMsg()
+            delay(someTime())
+            println("Async...")
+            endMsg()
+            obj("hola gioooooo")
+        }
+        println("result -> ${result.await()}")
+
+    }
+
+}
+data class obj(val i : String ="")
+
+fun cLaunch() {
+    //solo se ejecuta dentro de una funcion suspendida o dentro de una corrutina
+    /**Launch no emite un resultado solo ejectua el precesos puede ser un Trigger para otro evento
+     * ejemplo recoleccion de datos*/
+    runBlocking {
+        newTopic("Launch")
+        launch {
+            startMsg()
+            delay(someTime())
+            println("Launch...")
+            endMsg()
+        }
+    }
+}
+
+fun cRunBlocking() {
+    newTopic("Run blocking")
+    /**utilizar construccion para pruebas o educativo*/
+    runBlocking {
+        startMsg()
+        delay(someTime())
+        println("runBlocking...")
+        endMsg()
+    }
 }
 
 fun suspendFun() {
@@ -18,7 +64,7 @@ fun suspendFun() {
      * hasta  obtener un resultado y el hilo principal podra seguirse ejcutando*/
     //delay(someTime())
     GlobalScope.launch {
-    //delay proviene de una suspend fun
+        //delay proviene de una suspend fun
         delay(someTime())
     }
 }
